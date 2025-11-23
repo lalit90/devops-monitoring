82bd007f2dff591077e010608c309469c0558f4b
Lalit <LSaini@delphime.com>
2025-11-23 17:02:23 +0400

    add experiment

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)  # exposes /metrics automatically
 
+
 @app.route("/")
 def hello():
-    return "Hello from Python Service!"
+    return "Hello from Python Service!Experiment"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
